9963870600d387688fec297bbf864abdb30fb243
surajdudhankar <surajdudhankar96@gmail.com>
2025-02-14 22:38:05 +0530

    update hotel code

---
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -0,0 +1,9 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class hotel {
+        @GetMapping("/hotel")
+        public String getData() {return  "Please book your hotel ticket for New Delhi 35% discount" ; }}
