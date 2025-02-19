b88d213b4781dec7d0ed7df93755455c715abcf1
surajdudhankar <surajdudhankar96@gmail.com>
2025-02-19 15:04:53 +0530

    changes in flight booking 35 to 50%

---
+++ src/main/java/com/example/booking_ms/flight.java
@@ -0,0 +1,10 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please book your flight ticket for New Delhi 50% discount" ; }
+}
