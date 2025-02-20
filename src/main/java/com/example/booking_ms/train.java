fc207996010d7aa31b59e937772952bba9c9e7f7
surajdudhankar <surajdudhankar96@gmail.com>
2025-02-20 10:53:36 +0530

    changes in train ticket

--- src/main/java/com/example/booking_ms/train.java
+++ src/main/java/com/example/booking_ms/train.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class train {
+public class train
+ {
     @GetMapping("/train")
     public String getData() {return  "Please book your train ticket for New Delhi 50% discount" ; }
 }
